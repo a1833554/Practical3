c18e7a878f6a77bb26c9fbe21076f4b431b2d263
a1833554 <110015029+a1833554@users.noreply.github.com>
2022-08-17 14:11:26 +0930

    Add files via upload

---
+++ function-1-1.cpp
@@ -0,0 +1,28 @@
+//
+//  function-1-1.cpp
+//  
+//
+//  Created by Shea Cowan on 17/8/22.
+//
+#include <iostream>
+#include <string>
+
+void string_2d_copy(std::string first[][2], std::string second[][2], int n){
+    
+    
+    for (int i = 0; i< n; i++){
+        for(int j = 0; j< 2; j++){
+            second[i][j] = first[i][j];
+        }
+    }
+    
+    for (int i = 0; i< n; i++){
+        for(int j = 0; j< 2; j++){
+            if(j==1){
+            std::cout<<second[i][j];
+            }else{
+                std::cout<<second[i][j]<<" ";
+            }
+        }std::cout<<std::endl;
+    }
+}
---
+++ main-1-1.cpp
@@ -0,0 +1,21 @@
+//
+//  main-1-1.cpp
+//  
+//
+//  Created by Shea Cowan on 17/8/22.
+//
+#include <string>
+#include <iostream>
+
+int main(){
+    
+extern void string_2d_copy(std::string first[][2], std::string second[][2], int n) ;
+ 
+
+    std::string first[3][2] = {{"fish","dog"},{"mad","towl"},{"cat","frog"}};
+    std::string second[3][2];
+    
+    string_2d_copy(first,second,3);
+    
+    
+}
