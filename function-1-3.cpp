a5088781a32529c4dc6a4cdd99c6d927d42a9d30
a1833554 <110015029+a1833554@users.noreply.github.com>
2022-08-17 14:56:52 +0930

    Add files via upload

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -10,13 +10,21 @@
 
 extern void copy_integers(int old_array[],int new_array[],int length){
     
+    
+    int *oldarr = old_array;
+    int *newarr = new_array;
+    
+    for (int i = 0; i< length; i++){
+        *(newarr+i) = *(oldarr+i);
+    }
+    /*
     int *ptr;
     ptr = &old_array[0];
 
     new_array = ptr;
     
-
+*/
     for (int i = 0; i<length; i++){
-        std::cout<< new_array[i]<< " ";
+        std::cout<< *(newarr+i)<< " ";
     }std::cout<<std::endl;
 }
