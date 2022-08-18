8f1c207df3862f172932dbd7fd8dca068211bfb0
a1833554 <110015029+a1833554@users.noreply.github.com>
2022-08-18 13:24:24 +0930

    Add files via upload

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -15,7 +15,9 @@ void string_2d_copy(std::string first[][2], std::string second[][2], int n){
             second[i][j] = first[i][j];
         }
     }
-    
+}
+
+/*
     for (int i = 0; i< n; i++){
         for(int j = 0; j< 2; j++){
             if(j==1){
@@ -26,3 +28,5 @@ void string_2d_copy(std::string first[][2], std::string second[][2], int n){
         }std::cout<<std::endl;
     }
 }
+  */
+
