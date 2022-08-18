df375d0f8ec6ac943005b4e09d2c445cfc60df89
a1833554 <110015029+a1833554@users.noreply.github.com>
2022-08-18 13:28:43 +0930

    Add files via upload

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -11,7 +11,7 @@ void copy_doubles(double *old_array,double *new_array,int length){
     for (int i = 0; i< length; i++){
         *(new_array+i) = *(old_array+i);
     }
-
+}
     /*
     for (int i = 0; i< length; i++){
         if(i == length-1){
